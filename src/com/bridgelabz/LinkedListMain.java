5663b28d28737ae6c655bc791d6f1ed6a861aafe
akashp69 <akashpakhare499@gmail.com>
2022-12-12 00:29:16 +0530

    Added: Program For Deques The elements from Begging

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -2,17 +2,18 @@ package com.bridgelabz;
 
 public class LinkedListMain {
     /**
-     *This is main Method is Using for create que
+     *This is main Method is Using for deques elements
      */
-    public static void main(String[] args) {
+
+        public static void main(String[] args) {
             LinkedLists oparetionQueue = new LinkedLists();
             oparetionQueue.append(56);
             oparetionQueue.append(30);
             oparetionQueue.append(70);
 
             oparetionQueue.display();
+            oparetionQueue.dequeue();
         }
 
     }
-
 
